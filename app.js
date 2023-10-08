afd2e23fa316786fd183461cde65598f2f9b0789
Agszyn <128422475+Agszyn@users.noreply.github.com>
2023-10-08 21:34:51 +0200

    Add files via upload
    Signed-off-by: Agszyn <128422475+Agszyn@users.noreply.github.com>

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const observer = new IntersectionObserver((entries) => {
 
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
+            //} else {
+            //  entry.target.classList.remove("show");
         }
 
     });
